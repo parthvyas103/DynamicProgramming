e2a60a7403387b3469af6a9a55ee7603ac0c8e77
parthvyas103 <46166077+parthvyas103@users.noreply.github.com>
2022-06-27 11:41:50 +0530

    Create 120. Triangle.cpp

---
+++ 120. Triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
+        int n = triangle[m-1].size();
+        vector<vector<int>> dp(m, vector<int>(n, INT_MAX));
+        
+        for(int i = 0; i < n; i++)
+            dp[m-1][i] = triangle[m-1][i];
+        
+        for(int i = m-2; i >= 0; i--){
+            for(int j = 0; j < triangle[i].size(); j++){
+                dp[i][j] = min(dp[i+1][j], dp[i+1][j+1]) + triangle[i][j];
+            }
+        }
+        
+        return dp[0][0];
+    }
+};
